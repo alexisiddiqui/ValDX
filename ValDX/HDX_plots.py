f22171e1b0670a94095e541c6af249b7c6422b1b
alexisiddiqui <alexisiddiqui831@gmail.com>
2023-11-01 13:27:44 +0000

    completed HDX plots for matplotlib - want to create versions for plotly, also want to create some functions to do the plotting onto the structure

--- ValDX/HDX_plots.py
+++ ValDX/HDX_plots.py
@@ -338,6 +338,9 @@ def plot_peptide_dfracs(args: list, data: pd.DataFrame, times: list, segs: pd.Da
     plt.show()
 
 
+
+
+
 def plot_peptide_dfracs_errors(args: list, data: pd.DataFrame, top: mda.Universe, times: list, segs: pd.DataFrame, save=False, save_dir=None, expt_index=0):
         
     residues = segs
@@ -442,3 +445,300 @@ def plot_peptide_dfracs_errors(args: list, data: pd.DataFrame, top: mda.Universe
     plt.show()
 
 
+
+
+
+def plot_dfracs_error(args: list, data: pd.DataFrame, RMSF: list or np.ndarray, top: mda.Universe, times: list, segs: pd.DataFrame, save=False, save_dir=None, expt_index: int=0, abs=False):
+        
+    """Plot HDX deuterated fractions for each time point.
+    
+    Args:
+        *args: 
+            'expt' - experimental HDX deuterated fractions
+            'pred' - computed HDX deuterated fractions
+            'reweighted' - reweighted HDX deuterated fract
+            
+        data (pd.DataFrame): dataframe containing data to plot using dfs from helpful_funcs.py
+        RMSF (list or np.ndarray): RMSF of each residue from MD simulation
+        top (mda.Universe): topology of MD simulation
+        times (list): list of times to plot
+        segs (pd.DataFrame): dataframe containing peptide segment information
+        save (bool): whether to save the figure
+        save_dir (str): directory to save the figure in
+        expt_index (int): index of expt in args
+        abs (bool): whether to plot absolute error or not
+    """
+    if 'expt' == args:
+        return ValueError('expt must be included in args')
+    
+    expt = data[args[expt_index]].copy()
+
+    args = [arg for arg in args if arg != args[expt_index]]
+# calculate standard deviation of each residue from xtal structure bfactors
+
+    # residues = segs
+    # print(residues)
+
+    #create set of residue numbers from resstr 
+    # resnums = {residues.iloc[i, 0] for i in range(residues.shape[0])}
+
+
+    fig, axs = plt.subplots(len(times), 1, figsize=(12, 24))
+    for i, (ax, t) in enumerate(zip(axs, times)):
+        # plot RMSF
+        xs = np.arange(0, expt.iloc[:, 1].shape[0])
+        ys = RMSF
+        label = 'Scaled RMSF (Abs)'
+        # scale to 0-1
+        ys = (ys - ys.min())/(ys.max() - ys.min())
+
+        # add -ys to plot as bar chart
+        xs = np.concatenate((xs, xs))
+        ys = np.concatenate((ys, -ys))
+
+        # plot as bar chart
+        ax.bar(xs, ys, label=label, color='gray', alpha=0.3)
+
+        for arg in args:
+            # if arg in ['expt', 'pred', 'reweighted']:
+            df = data[arg].copy()
+            xs = np.arange(0, df.iloc[:, 1].shape[0])
+            ys = df.iloc[:, i]
+                # calculate absolute difference between ys and expt at each residue
+            if abs:
+                difference = np.abs(ys - expt.iloc[:, i])
+            else:
+                difference = ys - expt.iloc[:, i]
+            label = arg + ' difference from expt'
+            # line, = ax.plot(xs, difference, label=label)
+            # line_color = line.get_color()
+            ax.axhline(0, color='gray', linestyle='--')  # This line adds a horizontal line at y=0
+
+            ax.plot(xs, difference, label=label)
+            ax.set_title(f'Labeling time = {t} min')
+            ax.legend(loc='upper right')
+            ax.set_xticks(xs)
+            ax.set_xlim(xs[0], xs[-1])
+            ax.set_xticklabels(segs.iloc[:, 1], rotation=90)
+            ax.set_ylim(-1, 1)
+            # else:
+            #     print("Incorrect argument given. Please choose one or more of the following: 'expt' 'pred' 'reweighted'")
+    fig.text(0.5, 0.095, 'Residue', ha='center', fontsize=22)
+    fig.text(0.05, 0.5, 'HDX df absolute error from expt', va='center', rotation='vertical', fontsize=22)
+
+
+
+
+
+def plot_dfracs_compare(args: list, data: pd.DataFrame, times: list, save=False, save_dir=None, expt_index: int=0):
+        
+    """Plot HDX deuterated fractions for each time point.
+    
+    Args:
+        *args: 
+            'expt' - experimental HDX deuterated fractions
+            'pred' - computed HDX deuterated fractions
+            'reweighted' - reweighted HDX deuterated fractions
+        data (pd.DataFrame): dataframe containing data to plot using dfs from helpful_funcs.py
+        times (list): list of times to plot
+        save (bool): whether to save the figure
+        save_dir (str): directory to save the figure in
+        expt_index (int): index of expt in args
+
+    """
+    expt = data[args[expt_index]].copy()
+
+    all_diff_data = []
+    # plt.figure(figsize=(12, 6))
+    fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+
+    expt_means = []  # List to store mean experimental values at each time step
+
+    for i, t in enumerate(times):
+        expt_mean_at_t = np.mean(expt.iloc[:, i])
+        expt_means.append(expt_mean_at_t)
+        for arg in args:
+
+            df = data[arg].copy()
+            xs = np.arange(0, df.iloc[:, 1].shape[0])
+            ys = df.iloc[:, i]
+            # print(ys)
+            # Calculate absolute difference between ys and expt at each residue
+            difference = np.abs(ys - expt.iloc[:, i])
+            
+            # Storing differences with corresponding time and argument in the DataFrame
+            for j, d in enumerate(difference):
+                all_diff_data.append({'time': t, 'difference': d, 'type': arg, 'values': ys[j]})
+
+    # Convert list of dictionaries to DataFrame
+    df_differences = pd.DataFrame(all_diff_data)
+    print(df_differences)
+    # Plotting the violin plot
+    sns.boxplot(x='time', y='values', hue='type', data=df_differences)
+    plt.plot(range(0,4), expt_means, color='black', label='expt mean', linestyle='-', marker='o')
+
+    plt.title('HDX df empirical error from expt')
+    plt.xlabel('Labeling time (min)')
+    plt.ylabel('HDX Protection Factor')
+    plt.ylim(-0.05, 1.05)
+    plt.legend(loc='upper right')
+    plt.show()
+    fig.text(0.5, 0.095, 'Residue', ha='center', fontsize=22)
+    fig.text(0.05, 0.5, 'HDX df compare to expt', va='center', rotation='vertical', fontsize=22)
+
+
+
+
+def plot_dfracs_compare_hist(args: list, data: pd.DataFrame, times: list,  save=False, save_dir=None, expt_index: int=0):
+        
+    """Plot HDX deuterated fractions for each time point.
+    
+    Args:
+        *args: 
+            'expt' - experimental HDX deuterated fractions
+            'pred' - computed HDX deuterated fractions
+            'reweighted' - reweighted HDX deuterated fractions
+    """
+
+    expt = data[args[expt_index]].copy()
+    all_diff_data = []
+    expt_means = []  # List to store mean experimental values at each time step
+
+    for i, t in enumerate(times):
+        expt_mean_at_t = np.mean(expt.iloc[:, i])
+        expt_means.append(expt_mean_at_t)
+        for arg in args:
+
+            df = data[arg].copy()
+
+            xs = np.arange(0, df.iloc[:, 1].shape[0])
+            ys = df.iloc[:, i]
+            # print(ys)
+            # Calculate absolute difference between ys and expt at each residue
+            difference = np.abs(ys - expt.iloc[:, i])
+            
+            # Storing differences with corresponding time and argument in the DataFrame
+            for j, d in enumerate(difference):
+                all_diff_data.append({'time': t, 'difference': d, 'type': arg, 'values': ys[j]})
+
+    # Convert list of dictionaries to DataFrame
+    df_differences = pd.DataFrame(all_diff_data)
+
+    unique_times = df_differences['time'].unique()
+    fig, axes = plt.subplots(nrows=len(unique_times), figsize=(12, 6 * len(unique_times)))
+    # If only one time point, make axes iterable
+    if not isinstance(axes, np.ndarray):
+        axes = [axes]
+
+    # Iterate over each time and the corresponding axis
+    for idx,_ in enumerate(zip(axes, unique_times)):
+        ax, t = _
+        # Filter data for the current time
+        df_time = df_differences[df_differences['time'] == t]
+        
+        # Create a list to store histogram data for each type
+        histograms = []
+
+        for arg in args:
+            # Filter data by type (e.g., 'expt', 'pred', 'reweighted')
+            type_data = df_time[df_time['type'] == arg]['values']
+            
+            # Create histogram for the current type and append it to the histograms list
+            histograms.append(type_data)
+
+        # Plot histograms on the current axis
+        ax.hist(histograms, bins=10, label=args, histtype='bar' ,alpha=0.5)
+
+        ax.axvline(x=expt_means[idx], color='black', label='expt mean', linestyle='-')
+        ax.set_title(f'HDX df empirical error from expt at time {t} min')
+        ax.set_xlabel('HDX Protection Factor')
+        ax.set_ylabel('Count')
+        ax.legend(loc='upper right')
+
+    plt.tight_layout()
+    plt.show()
+            #     print("Incorrect argument given. Please choose one or more of the following: 'expt' 'pred' 'reweighted'")
+    fig.text(0.5, 0.095, 'Residue', ha='center', fontsize=22)
+    fig.text(0.05, 0.5, 'HDX df compare to expt', va='center', rotation='vertical', fontsize=22)
+
+
+
+
+
+def plot_dfracs_compare_hist_errors(args: list, data: pd.DataFrame, times: list,  save=False, save_dir=None, expt_index: int=0):
+        
+    """Plot HDX deuterated fractions for each time point.
+    
+    Args:
+        *args: 
+            'expt' - experimental HDX deuterated fractions
+            'pred' - computed HDX deuterated fractions
+            'reweighted' - reweighted HDX deuterated fractions
+    """
+
+    expt = data[args[expt_index]].copy()
+
+    args = [arg for arg in args if arg != args[expt_index]]
+
+    all_diff_data = []
+
+
+    expt_means = []  # List to store mean experimental values at each time step
+
+    for i, t in enumerate(times):
+        expt_mean_at_t = np.mean(expt.iloc[:, i])
+        expt_means.append(expt_mean_at_t)
+        for arg in args:
+
+            df = data[arg].copy()
+
+            xs = np.arange(0, df.iloc[:, 1].shape[0])
+            ys = df(arg).iloc[:, i]
+            # print(ys)
+            # Calculate absolute difference between ys and expt at each residue
+            difference = np.abs(ys - expt.iloc[:, i])
+            
+            # Storing differences with corresponding time and argument in the DataFrame
+            for j, d in enumerate(difference):
+                all_diff_data.append({'time': t, 'difference': d, 'type': arg, 'values': ys[j]})
+
+    # Convert list of dictionaries to DataFrame
+    df_differences = pd.DataFrame(all_diff_data)
+
+    unique_times = df_differences['time'].unique()
+    fig, axes = plt.subplots(nrows=len(unique_times), figsize=(12, 6 * len(unique_times)))
+    # If only one time point, make axes iterable
+    if not isinstance(axes, np.ndarray):
+        axes = [axes]
+
+    # Iterate over each time and the corresponding axis
+    for idx,_ in enumerate(zip(axes, unique_times)):
+        ax, t = _
+        # Filter data for the current time
+        df_time = df_differences[df_differences['time'] == t]
+        
+        # Create a list to store histogram data for each type
+        histograms = []
+
+        for arg in args:
+            # Filter data by type (e.g., 'expt', 'pred', 'reweighted')
+            type_data = df_time[df_time['type'] == arg]['difference']
+            
+            # Create histogram for the current type and append it to the histograms list
+            histograms.append(type_data)
+
+        # Plot histograms on the current axis
+        ax.hist(histograms, bins=10, label=args, alpha=0.3)
+
+
+        # ax.axvline(x=expt_means[idx], color='black', label='expt mean', linestyle='-')
+        ax.set_title(f'HDX df empirical error from expt at time {t} min')
+        ax.set_xlabel('HDX Protection Factor error')
+        ax.set_ylabel('Count')
+        ax.legend(loc='upper right')
+
+    plt.tight_layout()
+    plt.show()
+            #     print("Incorrect argument given. Please choose one or more of the following: 'expt' 'pred' 'reweighted'")
+    fig.text(0.5, 0.095, 'Residue', ha='center', fontsize=22)
+    fig.text(0.05, 0.5, 'HDX df compare to expt', va='center', rotation='vertical', fontsize=22)
